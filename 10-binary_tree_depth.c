211d42cf3f0c330ed864a0ca87396cddb2fd801e
Plaris-algedi <karchalmohammed@gmail.com>
2023-08-30 17:34:03 -0700

    Refactor the code

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,16 +9,16 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	int depth = 0;
-	binary_tree_t *node = tree->parent;
+	const binary_tree_t *node;
 
+	node = tree;
 	if (tree)
 	{
-		while (node)
+		while (node->parent)
 		{
 			depth++;
 			node = node->parent;
 		}
-		return (depth);
 	}
-	return (0);
+	return (depth);
 }
